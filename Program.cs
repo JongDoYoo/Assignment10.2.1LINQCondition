cc09bf20f6262743b6450714b1dc8f8cfc67e411
JongD <JongD@JONGDO>
2024-07-08 13:57:05 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Assignment10._2._1LINQCondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNum = { 2, -1, 3, -3, 10, -200  };
+
+            var numQuery = from num in myNum
+                           where num >=0
+                           select num;
+
+            foreach (int num in numQuery)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
